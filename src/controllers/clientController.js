628c0e5743a54dae353df0f0492c355d07fb60f4
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-09-25 10:25:01 -0600

    Modificacion de ClientController

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -1,6 +1,6 @@
 const Client = require('../models(client');
-const Client = require('../models/Client');
 
+//Obtener todos los clientes
 exports.getAllClients = async (req, res)=>{
 try{
     const Client = await Client.find();
@@ -9,6 +9,8 @@ try{
 res.status(500).json({error: 'Error al obtener los Clientes'});
 }
 };
+
+//Obtener un cliente por su ID
 exports.getAllClientById = async (req, res)=>{
     try{
 const client = await Client.findById(req.params.id);
@@ -18,5 +20,42 @@ if(!client){
 res.json(client);
     }catch(error){
 res.status(500).json({error: 'Error al obtener el cliente'});
+    }
+};
+//Crear un nuevo cliente
+exports.createClient = async (req, res)=>{
+    try {
+        const newClient = new Client(req, res);
+        const saveClient = await newClient.save();
+        res.status(201).json(saveClient);
+    } catch (error) {
+        res.status(500),json({error: 'Error al crear el cliente'})
+    }
+};
+//Actualizar un cliente existente 
+exports.updateClient = async(req, res)=>{
+    try {
+       const updateClient=await Client.findByIdAnUpdate(req.params.id, req.body,{
+        new: true,
+       });
+       if(!updateClient){
+        return res.status(404).json({error:'Cliente no encontrado'});
+       }
+       res.json(updateClient);
+    } catch (error) {
+        res.status(500).json({error: 'Error al actualizar el cliente'})
+    }
+};
+
+//Eliminar un cliente existente
+exports.deleteClient = async(req, res)=>{
+    try {
+        const deleteClient = await Client.findByIdAndRemove(req.params.id);
+        if(!deleteClient){
+            return res.status(404).json({error: 'Cliente noo encontrado'});            
+        }
+        res.json({message: 'Cliente eliminado correctamente'});
+    } catch (error) {
+        res.status(500).json({error: 'Error al eliminar el cliente'});
     }
 };
